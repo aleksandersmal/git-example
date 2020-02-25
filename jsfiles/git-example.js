30f6bf2f488307b7e6bad538189c4beb8bc8a9da
aleksander.smal <aleksander.smal@gmail.com>
2020-02-25 10:38:53 +0100

    added some js methods

--- jsfiles/git-example.js
+++ jsfiles/git-example.js
@@ -1,3 +1,11 @@
-function myFunction(p1, p2) {
+function alertMultiplication(p1, p2) {
     window.alert(p1 * p2);  
+}
+
+function alertAddition(p1, p2) {
+    window.alert(p1 + p2);  
+}
+
+function alertSubstraction(p1, p2) {
+    window.alert(p1 + p2);  
 }
